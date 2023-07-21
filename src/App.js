9d1aa4e7223873d3bd5fd918712d8974d012a0ae
YamJr <yamgrg2058@gmail.com>
2023-07-21 20:05:55 +0545

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [books, setBooks] = useState([])
+  const [loading, setLoading] = useState(true)
+  useEffect(()=>{
+       axios.get("https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=oZ0RJr1hqmfXJtCBIb1aQG8GBIRuqS0I").then(res=>{
+          console.log(res.data);
+          setBooks(res.data.results.lists)
+          setLoading(false);
+        }).catch(err=>console.log(err))
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+    <div className='container'>
+        <h2>Book Listing App</h2>
+        {loading ? "Loading...": ""}
+          {books.map((book,key)=>
+          <div className='row'>
+            <h3>{book.display_name}</h3>
+            {/* <div className='col-md-4'> */}
+              {book.books.map((_book,key)=>{
+              return <div className='col-md-4'>
+                  <img src={_book.book_image} />
+                  <p>Title:{_book.title}</p>
+                  <p>Author:{_book.author}</p>
+                  <p>Publisher:{_book.publisher}</p>
+              </div>
+           })}
+            </div> 
+          // </div>
+             )}
     </div>
   );
 }
